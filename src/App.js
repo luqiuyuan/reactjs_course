fb479e412f6cfe60b667d09a15e93000d836b079
luqiuyuan <lsun100@uottawa.ca>
2019-02-16 10:28:46 -0500

    Initialize repository

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import styles from './styles/App';
+import TextInput from './TextInput';
+import Button from './Button';
+
+class App extends Component {
+  render() {
+    return (
+      <div style={styles.container}>
+
+        <div style={styles.panel}>
+
+          <p style={styles.header}>BIG FISH</p>
+          
+          <TextInput placeholder="Email" />
+          <TextInput placeholder="Password" />
+          <TextInput placeholder="Name" />
+
+          <Button label="Signup" />
+
+          <div style={styles.placeholder} />
+
+          <div style={styles.footer}>
+            <p style={styles.footer_text}>Already have an account?</p>
+            <p style={styles.footer_link}>Login</p>
+          </div>
+
+        </div>
+
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/styles/TextInput.js
@@ -0,0 +1,20 @@
+import { COLOR_CRAB_SHELL_CYAN } from '../constants';
+
+const styles = {
+    container: {
+
+    },
+    input: {
+        width: 386,
+        fontSize: 25,
+        borderStyle: 'none',
+        outline: 'none',
+    },
+    line: {
+        borderBottomWidth: 1,
+        borderBottomColor: COLOR_CRAB_SHELL_CYAN,
+        borderBottomStyle: 'solid',
+    },
+};
+
+export default styles;
