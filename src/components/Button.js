1c9cb47191ca61d7bf3dcafe25a856c6c2fbc9f3
Zhe Xu <lyxuzhe@gmail.com>
2019-02-22 14:44:30 -0500

    add validation to login form

---
+++ src/components/Button.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+import styles from './styles/Button';
+
+export default class Button extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      hovered: false,
+    };
+  }
+
+  render() {
+    const { onClick } = this.props
+    return (
+      <button
+        onClick={onClick}
+        style={{ ...styles.button, opacity: this.state.hovered ? 0.5 : 1 }}
+        onMouseEnter={() => {
+          this.setState({ hovered: true });
+        }}
+        onMouseLeave={() => {
+          this.setState({ hovered: false });
+        }}>
+        <p style={styles.button_label}>{this.props.label}</p>
+      </button>
+    );
+  }
+
+}
--- src/constants.js
+++ src/constants.js
@@ -5,3 +5,5 @@ export const COLOR_SILVER_WHITE = '#E9E7EF';
 export const COLOR_YAQING = '#424C50';
 
 export const COLOR_CRAB_SHELL_CYAN = '#BBCDC5';
+
+export const COLOR_ERROR_RED = '#FF2121';
