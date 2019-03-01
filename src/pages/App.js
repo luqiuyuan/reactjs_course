3a954a7af7b50533becc111aa8459b8caeffba13
luqiuyuan <lsun100@uottawa.ca>
2019-03-01 15:59:59 -0500

    Move styles App to its appropriate location

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import styles from '../styles/App';
+import styles from './styles/App';
 import TextInput from '../components/TextInput';
 import Button from '../components/Button';
 import WhiteBlank from '../components/WhiteBlank';
