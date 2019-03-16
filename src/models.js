5adee437865df4e0eac1a78b63c8d4c5d0268a50
luqiuyuan <lsun100@uottawa.ca>
2019-03-15 20:37:10 -0400

    Fix a bug

--- src/models.js
+++ src/models.js
@@ -11,7 +11,39 @@ export const user_token = {
   },
   effects: (dispatch) => ({
     create(payload, state) {
-      // networking stuff here
+      let request = axios({
+        method: 'post',
+        url: SERVER_ADDRESS + '/user_tokens',
+        data: {
+          credential: {
+            email: payload.email,
+            password: payload.password,
+          }
+        },
+        validateStatus: function (status) {
+          return (status >= 200 && status < 300) || (status >= 400 && status < 500);
+        },
+      });
+  
+      request.then((response) => {
+        if (response.status == 201) {
+          dispatch.user_token.set(response.data.user_token);
+  
+          payload && payload.success_callback && payload.success_callback();
+        } else if (response.status == 400) {
+          let first_error = response.data.errors[0];
+          if (first_error.code == 'invalid_credential') {
+            Popup.warn("Email or password is incorrect! Please try again");
+          } else {
+            Popup.warn("Something expected happened T_T Please contact admin@bigfish.ca. (error code is " + first_error.code + ")");
+          }
+        } else {
+          Popup.warn("Something expected happened T_T Please contact admin@bigfish.ca. (status is " + response.status + ")");
+        }
+      })
+      .catch((error) => {
+        Popup.warn("Something expected happened T_T Please contact admin@bigfish.ca. (error is " + error + ")");
+      });
     }
   }),
 };
