67932d56e285fc0901fb5040cdb8fe9280e968d0
luqiuyuan <lsun100@uottawa.ca>
2019-05-31 22:25:12 -0400

    Add lecture00

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,8 +1,19 @@
 import React from 'react';
-import defaultAvatar from '../assets/imgs/avatar_default.jpg'
+import defaultAvatar from '../assets/imgs/avatar_default.jpg';
+import { Link, Route, Switch, Redirect } from 'react-router-dom';
 
-export default function Avatar({size, src}){
-  return <img alt={'avatar'} style={styles.avatar(size)} src={src || defaultAvatar}/>
+export default class Avatar extends Component {
+
+  render() {
+    return (
+      <img
+        alt={'avatar'}
+        style={styles.avatar(this.props.size)}
+        src={this.props.src || defaultAvatar}
+        onClick={} />
+    )
+  }
+
 }
 
 Avatar.defaultProps = {
